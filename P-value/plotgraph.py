827fdc9d16ae774dc7c64d2b897c3216641633a0
Luyi Tian <tlytiger@hotmail.com>
2015-07-26 23:02:27 +1000

    new

--- P-value/plotgraph.py
+++ P-value/plotgraph.py
@@ -1,21 +1,21 @@
 #P-value
 import matplotlib.pyplot as plt 
 from random import gauss 
-
+'''
 #######
 X_ran,Y_ran = [gauss(0,1) for i in range(500)],[gauss(0,1) for i in range(500)]
 #######
 #plt.plot(X_ran,Y_ran,'x')
 #plt.show()
-
+'''
 #######
 X_col = [gauss(0,1) for i in range(500)]
-Y_col = [gauss(0,1)+x for x in X_col]
+Y_col = [gauss(0,1)+0.3*x for x in X_col]
 #######
-#plt.plot(X_col,Y_col,'x')
-#plt.show()
+plt.plot(X_col,Y_col,'x')
+plt.show()
 
-
+'''
 import numpy as np
 import matplotlib.mlab as mlab
 import math
@@ -27,3 +27,4 @@ x = np.linspace(-6,6,100)
 plt.plot(x,mlab.normpdf(x,mean,sigma))
 
 plt.show()
+'''
